725b6c3dc66e971247d5a737872fc714abdb9232
Patrick Reis <patrickbernardor@gmail.com>
2021-12-17 01:39:52 -0300

    [FEAT] Redirect difficulty page

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { AppContainer } from "./styles";
+import { useNavigate  } from "react-router-dom";
+
 export default function App(){
+	const [name, setName] = useState()
+	const navigate = useNavigate();
+	function onSubmit(e){
+		localStorage.setItem("name",name);
+		navigate('/difficulty');
+		e.preventDefault();
+
+	}
 	return(
 		<AppContainer>
 			<h1>Seja <strong>bem vindo</strong>,<br/>Digite seu nome para começar</h1>
-			<form>
-			<input placeholder="ex: Linus Torvalds"/>
+			<form onSubmit={onSubmit}>
+			<input placeholder="ex: Linus Torvalds" onChange={e => setName(e.target.value)}/>
 			</form>
 		</AppContainer>
 	);

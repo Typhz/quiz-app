9436bd841f74d6b3a883324928b9d4f3d0d93282
Patrick Reis <patrickbernardor@gmail.com>
2021-12-17 02:05:13 -0300

    [FEAT] Connect API

--- src/pages/Difficulty/index.js
+++ src/pages/Difficulty/index.js
@@ -1,14 +1,39 @@
 import React from "react";
 import {AppContainer, Difficulty } from "./styles";
+import { useNavigate } from "react-router-dom";
+import api from "../../services/api";
 
 export default function App(){
+  const navigate = useNavigate();
+  async function DifficultySelect(difficulty){
+    api.get('/questions', {
+      params: {
+        apiKey: "x5yYMkHgQ0xhz7Q7RD1CfTQESV5gXkBwlfcuNFed",
+        difficulty: difficulty,
+        limit: 10,
+      }
+    })
+    .then(function (response) {
+      // handle success
+      localStorage.setItem("quiz",JSON.stringify(response));
+      navigate('/quiz');
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });  
+  }
 	return(
 		<AppContainer>
 			<h1>Selecione um nivel <br/>de dificuldade</h1>
       <div className="list-difficulty">
-        <Difficulty active>Fácil</Difficulty>
-        <Difficulty>Médio</Difficulty>
-        <Difficulty>Dificil</Difficulty>
+        <Difficulty onClick={() => DifficultySelect('easy')} active>Fácil</Difficulty>
+        <Difficulty onClick={() => DifficultySelect('medium')} >Médio</Difficulty>
+        <Difficulty onClick={() => DifficultySelect('hard')} >Dificil</Difficulty>
       </div>
 		</AppContainer>
 	);

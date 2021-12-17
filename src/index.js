119245baf7624a714ea85ef178259f4522e2fa21
Patrick Reis <patrickbernardor@gmail.com>
2021-12-17 01:23:51 -0300

    [FEAT] Add globalStyles index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
+import GlobalStyle from './styles/globalStyles'
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')

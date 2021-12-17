542abaa00baf2e82ae20d8e1b3da283f46688a01
Patrick Reis <patrickbernardor@gmail.com>
2021-12-17 16:55:35 -0300

    [FIX] Adjust responsive

--- src/pages/Difficulty/styles.js
+++ src/pages/Difficulty/styles.js
@@ -3,13 +3,12 @@ import colors from '../../styles/colors'
 export const AppContainer = styled.div`
 	min-height: 100vh;
   text-align: center;
-	/* padding: 0 12rem; */
 	flex-direction: column;
   align-items: center;
 	justify-content: center;
 	display: flex;
   .list-difficulty{
-    width: 15%;
+    min-width: 15%;
     flex-direction: row;
     justify-content: center;
     display: flex;
@@ -22,5 +21,5 @@ export const Difficulty = styled.button`
     border: ${(props) => props.active ? `2px solid ${colors.secondary}` : 'none'};
     border-radius: 6px;
     padding: 1rem 2rem;
-    margin: 1rem 2rem;
+    margin: 1rem 0.4rem;
 `

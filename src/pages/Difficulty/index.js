be5341764f7a28e9b82c63d147a579e28c7ddceb
Patrick Reis <patrickbernardor@gmail.com>
2021-12-18 23:36:34 -0300

    [REFACTOR] Fix indentation

--- src/pages/Difficulty/index.js
+++ src/pages/Difficulty/index.js
@@ -1,40 +1,50 @@
 import React from "react";
-import {AppContainer, Difficulty } from "./styles";
+import { AppContainer, Difficulty } from "./styles";
 import { useNavigate } from "react-router-dom";
 import api from "../../services/api";
 
-export default function App(){
+export default function App() {
   const navigate = useNavigate();
-  async function DifficultySelect(difficulty){
-    api.get('/questions', {
-      params: {
-        apiKey: "x5yYMkHgQ0xhz7Q7RD1CfTQESV5gXkBwlfcuNFed",
-        difficulty: difficulty,
-        limit: 10,
-      }
-    })
-    .then(function (response) {
-      // handle success
-      localStorage.setItem("quiz",JSON.stringify(response));
-      navigate('/quiz');
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });  
+  async function DifficultySelect(difficulty) {
+    api
+      .get("/questions", {
+        params: {
+          apiKey: "x5yYMkHgQ0xhz7Q7RD1CfTQESV5gXkBwlfcuNFed",
+          difficulty: difficulty,
+          limit: 10,
+        },
+      })
+      .then(function (response) {
+        // handle success
+        localStorage.setItem("quiz", JSON.stringify(response));
+        navigate("/quiz");
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
   }
-	return(
-		<AppContainer>
-			<h1>Selecione um nivel <br/>de dificuldade</h1>
+  return (
+    <AppContainer>
+      <h1>
+        Selecione um nivel <br />
+        de dificuldade
+      </h1>
       <div className="list-difficulty">
-        <Difficulty onClick={() => DifficultySelect('easy')} active>Fácil</Difficulty>
-        <Difficulty onClick={() => DifficultySelect('medium')} >Médio</Difficulty>
-        <Difficulty onClick={() => DifficultySelect('hard')} >Dificil</Difficulty>
+        <Difficulty onClick={() => DifficultySelect("easy")} active>
+          Fácil
+        </Difficulty>
+        <Difficulty onClick={() => DifficultySelect("medium")}>
+          Médio
+        </Difficulty>
+        <Difficulty onClick={() => DifficultySelect("hard")}>
+          Dificil
+        </Difficulty>
       </div>
-		</AppContainer>
-	);
+    </AppContainer>
+  );
 }

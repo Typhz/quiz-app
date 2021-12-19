95df2b1b0c6c29b5bc234e0adb069f21863a350d
Patrick Reis <patrickbernardor@gmail.com>
2021-12-18 23:35:53 -0300

    [REFACTOR] Fix indentation

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,23 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { AppContainer } from "./styles";
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export default function App(){
-	const [name, setName] = useState()
-	const navigate = useNavigate();
-	function onSubmit(e){
-		localStorage.setItem("name",name);
-		navigate('/difficulty');
-		e.preventDefault();
-
-	}
-	return(
-		<AppContainer>
-			<h1>Seja <strong>bem vindo</strong>,<br/>Digite seu nome para começar</h1>
-			<form onSubmit={onSubmit}>
-			<input placeholder="ex: Linus Torvalds" onChange={e => setName(e.target.value)}/>
-			</form>
-		</AppContainer>
-	);
-	
+export default function App() {
+  const [name, setName] = useState();
+  const navigate = useNavigate();
+  function onSubmit(e) {
+    localStorage.setItem("name", name);
+    navigate("/difficulty");
+    e.preventDefault();
+  }
+  return (
+    <AppContainer>
+      <h1>
+        Seja <strong>bem vindo</strong>,<br />
+        Digite seu nome para começar
+      </h1>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="ex: Linus Torvalds"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+    </AppContainer>
+  );
 }

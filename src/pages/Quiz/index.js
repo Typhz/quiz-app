d0dd1a36b5cb650c9473514894c7863ffd7f0bf0
Patrick Reis <patrickbernardor@gmail.com>
2021-12-21 04:31:40 -0300

    [FEAT] Finish quiz page

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,20 +1,129 @@
-import React from "react";
-import {AppContainer, QuizInfo, QuestionHeader, Answers } from "./styles";
-export default function App(){
+import React, { useState } from "react";
+import {
+  AppContainer,
+  ContainerQuiz,
+  QuizInfo,
+  QuestionHeader,
+  Answers,
+  FinishModel,
+  Answer,
+  Skip,
+  Back,
+} from "./styles";
+import { useNavigate } from "react-router-dom";
+import winImg from "../../assets/win.svg";
+
+function Quiz() {
+  const navigate = useNavigate();
   const name = localStorage.getItem("name");
-	return(
-		<AppContainer>
-			<QuestionHeader>
-			<QuizInfo>
-				<strong>{name}</strong>
-				<span> 1 de 10</span>
-			</QuizInfo>
-				<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut atque beatae possimus, vero assumenda quia. Illum modi officia rerum ipsum dolorem tempore eaque atque reiciendis dolor veniam quidem, expedita esse?</h4>
-			</QuestionHeader>
-			<Answers>
-        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</div>
-			</Answers>
-		</AppContainer>
-	);
-	
+  const quiz = localStorage.getItem("quiz");
+  const jsonQuiz = JSON.parse(quiz);
+  const questions = jsonQuiz.data;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [isFinish, setIsFinish] = useState(false);
+  const [score, setScore] = useState(0);
+  const [markedQuestion, setMarkedQuestion] = useState([]);
+  const nextQuestion = currentQuestion + 1;
+  const backQuestion = currentQuestion - 1;
+  const skip = () => {
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+  const back = () => {
+    if (nextQuestion <= questions.length) {
+      setCurrentQuestion(backQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+  const handleRemakeTest = () => {
+    navigate("/difficulty");
+  };
+  const handleAnswerOptionClick = (answer_) => {
+    if (answer_ === jsonQuiz.data[currentQuestion].correct_answer) {
+      setScore(score + 1);
+    }
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+      let newElement = [answer_];
+      setMarkedQuestion([...markedQuestion, newElement]);
+    } else {
+      setCurrentQuestion(nextQuestion);
+      let newElement = [answer_];
+      setMarkedQuestion([...markedQuestion, newElement]);
+      setShowScore(true);
+    }
+  };
+  function handleViewAnswer() {
+    setScore(0);
+    setShowScore(false);
+    setCurrentQuestion(0);
+    setIsFinish(true);
+  }
+
+  return (
+    <AppContainer>
+      <ContainerQuiz>
+        {showScore ? (
+          <FinishModel>
+            <img src={winImg} width={200} alt="You Win" />
+            <h2>Você completou o Quiz</h2>
+            <h2>Acertou {score} de 10</h2>
+            <button onClick={handleRemakeTest}>Refazer Teste</button>
+            <span onClick={handleViewAnswer}>Gabarito</span>
+          </FinishModel>
+        ) : (
+          <>
+            <progress id="file" value={currentQuestion} max="9"></progress>
+            <QuestionHeader>
+              {isFinish ? <Back onClick={back}>Voltar</Back> : <></>}
+              {isFinish ? <Skip onClick={skip}>Avançar</Skip> : <></>}
+              <QuizInfo>
+                <strong>{name}</strong>
+                <span> {currentQuestion + 1} de 10</span>
+              </QuizInfo>
+              <h4>{jsonQuiz.data[currentQuestion].question}</h4>
+            </QuestionHeader>
+            <Answers>
+              {Object.keys(jsonQuiz.data[currentQuestion].answers).map(
+                (key, index) => {
+                  if (jsonQuiz.data[currentQuestion].answers[key] != null) {
+                    return (
+                      <Answer
+                        id={key}
+                        key={index}
+                        checked={
+                          key == markedQuestion[currentQuestion]
+                            ? "selected"
+                            : "vaca"
+                        }
+                        onClick={() => handleAnswerOptionClick(key)}
+                        disabled={isFinish}
+                        isTrue={
+                          key ===
+                            jsonQuiz.data[currentQuestion].correct_answer &&
+                          isFinish === true
+                            ? true
+                            : false
+                        }
+                      >
+                        {jsonQuiz.data[currentQuestion].answers[key]}
+                        <br />
+                      </Answer>
+                    );
+                  }
+                }
+              )}
+            </Answers>
+          </>
+        )}
+      </ContainerQuiz>
+    </AppContainer>
+  );
 }
+
+export default Quiz;
--- src/pages/Quiz/styles.js
+++ src/pages/Quiz/styles.js
@@ -1,59 +1,148 @@
 import styled from "styled-components";
-import colors from '../../styles/colors'
-export const AppContainer = styled.div`
-	color: ${colors.fg};
-	min-height: 100vh;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	display: flex;
+import colors from "../../styles/colors";
+export const AppContainer = styled.section`
+  height: 90vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  display: flex;
+  progress {
+    border: none;
+    width: 100%;
+    height: 6px;
+    left: 0;
+    top: 0;
+    position: absolute;
+  }
+  progress::-webkit-progress-bar {
+    background-color: transparent;
+  }
+  progress::-webkit-progress-value {
+    background-color: #787af6;
+  }
+  progress::-moz-progress-bar {
+    /* style rules */
+  }
+`;
+export const ContainerQuiz = styled.div`
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  display: flex;
 `;
 
-
 export const QuizInfo = styled.div`
-	background-color: ${colors.secondary};
-	color: white;
-	border-radius: 10rem;
-	padding: 0.6rem 4rem;
-	top: -15%;
-	position: absolute
-`
+  background-color: ${colors.secondary};
+  color: white;
+  border-radius: 10rem;
+  padding: 0.6rem 3rem;
+  top: -14%;
+  position: absolute;
+`;
 
 export const QuestionHeader = styled.div`
-	background-color: white;
-	width: 60%;
-	height: 20vh;
-	padding: 0 2rem;
-	border-radius: 6px;
-	justify-content: center;
-	align-items: center;
-	word-wrap: break-word;
-	display: flex;
-	position: relative;
-
-`
+  background-color: white;
+  width: 70vw;
+  min-height: 20vh;
+  padding: 0 2rem;
+  border-radius: 6px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  position: relative;
+  @media only screen and (max-width: 950px) {
+    padding: 2rem 2rem;
+    position: initial;
+  }
+`;
 
 export const Answers = styled.div`
-	width: 60%;
-	margin-top: 2%;
-	min-height: auto;
-	justify-content: center;
-	grid-gap: 5%;
-	grid-auto-rows: 100px;
-	grid-template-columns: repeat(auto-fill, 47%);
-	display: grid;
+  width: 70vw;
+  height: 20vh;
+  margin: 0.8rem 0;
+  column-gap: 2rem;
+  row-gap: 0.8rem;
+  grid-template-columns: repeat(2, 1fr);
+  display: grid;
+  @media only screen and (max-width: 950px) {
+    grid-template-columns: repeat(1, 1fr);
+    grid-row-gap: 3%;
+  }
+`;
 
-	div{
-		background-color: white;
-		margin: 1rem 0;
-		border-radius: 6px;
-		justify-content: center;
-		align-items: center;
-		display: flex;
-		transition: 200ms;
-	}
-	div:hover{
-		transform: scale(0.98);
-	}
+export const Answer = styled.button`
+  background: ${(props) =>
+    props.isTrue
+      ? "#70AF85;"
+      : props.checked === "selected"
+      ? "#F05454;"
+      : props.disabled
+      ? "#dbdbdb;"
+      : "white;"}
+  color: ${(props) => (props.isTrue ? "white" : "black")};
+  height: 5rem;
+  padding: 2rem 2rem;
+  border-radius: 6px;
+  border: none;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  cursor: pointer;
+`;
+export const FinishModel = styled.div`
+  background: white;
+  min-width: 40%;
+  min-height: 60%;
+  padding: 0 1rem;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  display: flex;
+  border-radius: 6px;
+  position: absolute;
+  button {
+    background-color: ${colors.secondary};
+    color: white;
+    border-radius: 10rem;
+    border: none;
+    margin: 0.5rem 0;
+    padding: 0.5rem 4rem;
+    cursor: pointer;
+  }
+  span {
+    color: gray;
+    font-size: 0.8rem;
+    cursor: pointer;
+  }
+`;
 
-`
+export const Skip = styled.button`
+  background-color: ${colors.secondary};
+  color: white;
+  border: none;
+  border-radius: 10rem;
+  padding: 0.6rem 0.6rem;
+  top: 0%;
+  right: 0;
+  cursor: pointer;
+  position: absolute;
+  @media only screen and (max-width: 950px) {
+    top: 2%;
+    right: 2%;
+  }
+`;
+export const Back = styled.button`
+  background-color: ${colors.secondary};
+  color: white;
+  border: none;
+  border-radius: 10rem;
+  padding: 0.6rem 0.6rem;
+  cursor: pointer;
+  top: -14%;
+  left: 0;
+  position: absolute;
+  @media only screen and (max-width: 950px) {
+    top: 2%;
+    left: 2%;
+  }
+`;
